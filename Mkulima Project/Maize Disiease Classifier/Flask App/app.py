17362b14c3453699c60db4fd1521a62f94b54a5c
J. Olang <64926283+e-olang@users.noreply.github.com>
2022-09-20 11:12:17 +0300

    .

---
+++ Mkulima Project/Maize Disiease Classifier/Flask App/app.py
@@ -0,0 +1,9 @@
+# Set 'vission' as the conda environment to run python scripts
+#!/Users/olang/opt/miniconda3/envs/vission/bin/python
+
+# import libraries
+#from PIL import Image
+
+f = open("demo_file.txt", "a")
+f.write("I added a new line!\n")
+f.close()
